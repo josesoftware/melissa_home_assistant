c07a2852b5d0af7684e1d2179de1b418e2d13b43
José Antonio López Navarro <joseantonio.lopeznavarro93@gmail.com>
2021-03-19 17:45:15 +0100

    Animacion fade del despertar de melissa

--- modules/led_module_library/melissa_led_pattern.py
+++ modules/led_module_library/melissa_led_pattern.py
@@ -151,8 +151,8 @@ class MelissaLedPattern(object):
                 pixels[i * 4 + 2] = wheel[1] * (0.01 * o)
                 pixels[i * 4 + 3] = wheel[2] * (0.01 * o)
             
-            ## Duerme 0,1s
-            time.sleep(0.1)
+            ## Duerme 0,01s
+            time.sleep(0.01)
 
             ## Mostramos color
             self.show(pixels)
